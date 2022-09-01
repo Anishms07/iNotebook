b1b8a0235434411a6dfbeff06b7f706efacbb10b
Anishms07 <anishms666@gmail.com>
2022-09-01 12:50:59 +0530

    first commit

---
+++ src/components/About.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <p>
+        Artists often use large notebooks, which include wide spaces of blank
+        paper appropriate for drawing. Similarly composers utilize notebooks for
+        writing their lyrics. Lawyers use rather large notebooks known as legal
+        pads that contain lined paper (often yellow) and are appropriate for use
+        on tables and desks. These horizontal lines or "rules" are sometimes
+        classified according to their space apart with "wide rule" the farthest,
+        "college rule" closer, "legal rule" slightly closer and "narrow rule"
+        closest, allowing more lines of text per page. When sewn into a
+        pasteboard backing, these may be called composition books, or in smaller
+        signatures may be called "blue books" or exam books and used for essay
+        exams. Various notebooks are popular among students for taking notes.
+        The types of notebooks used for school work are single line, double
+        line, four line, square grid line etc. These notebooks are also used by
+        students for school assignments (homeworks) and writing projects. In
+        contrast, journalists prefer small, hand-held notebooks for portability
+        (reporters' notebooks), and sometimes use shorthand when taking notes.
+        Scientists and other researchers use lab notebooks to document their
+        experiments. The pages in lab notebooks are sometimes graph paper to
+        plot data. Police officers are required to write notes on what they
+        observe, using a police notebook. Land surveyors commonly record field
+        notes in durable, hard-bound notebooks called "field books." Coloring
+        enthusiasts use coloring notebooks for stress relief. The pages in
+        coloring notebooks contain different adult coloring pages.[9] Students
+        take notes in notebooks, and studies suggest that the act of writing (as
+        opposed to typing) improves learning.[10] Notebook pages can be recycled
+        via standard paper recycling. Recycled notebooks are available,
+        differing in recycled percentage and paper quality.
+      </p>
+    </div>
+  );
+};
+
+export default About;
